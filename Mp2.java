2505a2c9e16ff6c4b28928b6fd537ffa3ecdd2d3
ntysdd <ntysdd@gmail.com>
2025-02-03 21:54:55 +0800

    Update Mp2.java

--- Mp2.java
+++ Mp2.java
@@ -1,38 +1,45 @@
-import java.math.BigInteger;
+import java.math.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.*;
 
 public class Mp2 {
     static boolean isPrime(BigInteger n) {
         return n.signum() > 0 && n.isProbablePrime(100);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        for (int i = 1; i <= 1000; i++) {
-            if (!isPrime(BigInteger.valueOf(i))) {
-                continue;
-            }
+        ExecutorService pool = Executors.newFixedThreadPool(4);
 
-            System.out.print(i);
-            f(i);
+        List<Future<String>> futureList = new ArrayList<>();
 
-            System.out.println();
+        IntStream.range(1, 1000)
+            .filter(x -> isPrime(BigInteger.valueOf(x)))
+            .forEach(i -> futureList.add(pool.submit(() -> f(i))));
+
+        for (Future<String> future : futureList) {
+            System.out.println(future.get());
         }
 
+        pool.shutdown();
     }
 
-    static void f(int n) {
-        int count = 0;
+    static String f(int n) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(n);
 
+        int count = 0;
 
         for (int i = 2; ; i++) {
             BigInteger d = BigInteger.valueOf(i - 1);
             BigInteger x = BigInteger.valueOf(i).pow(n)
                 .subtract(BigInteger.ONE).divide(d);
             if (isPrime(x)) {
-                System.out.print(" " + i);
+                sb.append(" " + i);
                 count++;
                 if (count == 100) {
-                    return;
+                    return sb.toString();
                 }
             }
         }
